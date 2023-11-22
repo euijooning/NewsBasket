80b2c780feb2b0bccc7657502649019a1da3b0c3
EUIJOON <euijoonism@gmail.com>
2023-11-22 18:05:17 +0900

    [feat] 뉴스 보여주기 기초 구현

---
+++ main.js
@@ -0,0 +1,41 @@
+const API_KEY = "4e15c1245600477d85258dae3f69da39";
+let newsList = [];
+
+async function getLatestNews() {
+  // url 설정
+  let url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+
+  const response = await fetch(url); // 이 url 주소로 데이터 가져오기
+  const data = await response.json();
+
+  newsList = data.articles; // 값 확정
+  // 여기서 render()를 불러야 한다.
+  render();
+  console.log("info", newsList);
+}
+
+// 뉴스 정보 가져오기
+const render = () => {
+    // const는 재할당이 안 되므로 바로 그냥 값을 줘버린다.
+  const newsHTML = newsList.map(
+    (news) => `<div class="row news">
+    <div class="col-lg-4"> 
+        <img class="news-img" src=${news.urlToImage}>
+    </div>
+    <div class="col-lg-8">
+        <h2>${news.title}</h2>
+        <p>
+            ${news.description}
+        </p>
+        <div>
+            ${news.source.name} * ${news.publishedAt}
+        </div>
+    </div>
+</div>`
+  );
+  document.getElementById("news-posts").innerHTML = newsHTML;
+};
+
+getLatestNews(); // 함수 실행
