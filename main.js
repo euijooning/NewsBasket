e795dadf936a750b60ec7e173b40ab4f44fb0d0b
EUIJOON <euijoonism@gmail.com>
2023-11-22 20:13:06 +0900

    [feat] 카테고리별 뉴스 가져오기 3 - 데이터 추출해서 가져오는 것까지 기능 완성(진짜 커밋)

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ async function getLatestNews() {
     `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   );
 
-  const response = await fetch(url); // 이 url 주소로 데이터 가져오기
-  const data = await response.json();
+  const response = await fetch(url); // 이 url 주소로부터 데이터 가져오기
+  const data = await response.json(); // json 형태로 추출
 
   newsList = data.articles; // 값 확정
   // 여기서 render()를 불러야 한다.
@@ -26,7 +26,12 @@ async function getNewsByCategory(event) {
     const category = event.target.textContent.toLowerCase();
   console.log("category", category);
 
-  const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`);
+  const url = new URL(
+    `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
+  );
+  const response = await fetch(url); // API 엔드포인트로 HTTP 요청을 보내고 응답을 기다림
+  const data = await response.json(); // 2. HTTP 응답에서 JSON 데이터를 추출하고, 다시 기다림.
+  console.log("data_", data);
 }
 
 // 뉴스 정보 가져오기
